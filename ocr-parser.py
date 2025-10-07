06b00c50bdb4f9d57826bdb73c4619e0286067cd
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2025-10-07 03:08:43 -0400

    wrote function to get image file path

--- ocr-parser.py
+++ ocr-parser.py
@@ -2,5 +2,22 @@ import os
 from PIL import Image
 import pytesseract
 
+def get_file_path(prompt_msg: str="Enter file path") -> str:
+
+    msg = f"{prompt_msg}: " # Format prompt message
+
+    # Prompt user for file path, remove quotes if any
+    path = input(msg).replace('"', '').replace("'", '')
+
+    # Validate file path
+    while not os.path.isfile(path):
+        print("Invalid file path. Please try again.")
+        path = input(msg).replace('"', '').replace("'", '')
+
+    path = os.path.abspath(path) # Convert to absolute path
+
+    return path
+
 if "__main__" == __name__:
-    print("Hello World!")
+    img_file_path = get_file_path("Enter the image file path")
+    print(img_file_path)
